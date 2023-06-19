a0783c3a5044ca1a0e41495e3ee1dfc677e58bb6
LuzHelber <helbersap@gmail.com>
2023-06-19 17:24:31 -0300

    implementado o validation do email

--- src/main/java/br/com/helber/testecliente/domain/Cliente.java
+++ src/main/java/br/com/helber/testecliente/domain/Cliente.java
@@ -2,6 +2,7 @@ package br.com.helber.testecliente.domain;
 
 import br.com.helber.testecliente.infra.repository.DadosAlteracaoClientes;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -22,6 +23,7 @@ public class Cliente {
     private String telefone;
 
     @Column
+    @Email
     private String email;
 
     public Cliente(DadosCadastroClientes dados) {
